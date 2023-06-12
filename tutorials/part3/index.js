ed0938209b470816f7b04be3181b1d3c28bded6f
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-12 22:11:25 +0800

    tutorial: 3a

---
+++ tutorials/part3/index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const app = express();
+
+const requestLogger = (req, res, next) => {
+  console.log("Method:", req.method);
+  console.log("Path:", req.path);
+  console.log("Body:", req.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "Unknown endpoint" });
+};
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(unknownEndpoint);
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (!note) {
+    res.status(404).end();
+  }
+
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const noteToDelete = notes.find((note) => note.id === id);
+
+  if (!noteToDelete) {
+    return res.status(404).end();
+  }
+
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "missing content",
+    });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  };
+
+  notes = notes.concat(note);
+
+  res.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
