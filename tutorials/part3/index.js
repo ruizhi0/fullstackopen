e3714a9d7df88f15e2afd4ab4a1a035ee698c27f
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-17 19:51:00 +0800

    feat: replace in-memory approach with database

--- tutorials/part3/index.js
+++ tutorials/part3/index.js
@@ -19,24 +19,6 @@ app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
@@ -48,32 +30,24 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  if (!note) {
-    res.status(404).end();
-  }
-
-  res.json(note);
+  Note.findById(req.params.id)
+    .then((note) => {
+      res.json(note);
+    })
+    .catch((error) => {
+      res.status(404).end();
+    });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const noteToDelete = notes.find((note) => note.id === id);
-
-  if (!noteToDelete) {
-    return res.status(404).end();
-  }
-
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
+  Note.findByIdAndDelete(req.params.id).then((deletedNote) => {
+    if (deletedNote) {
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
+  });
 });
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.post("/api/notes", (req, res) => {
   const body = req.body;
@@ -84,15 +58,20 @@ app.post("/api/notes", (req, res) => {
     });
   }
 
-  const note = {
-    id: generateId(),
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-  };
+  });
 
-  notes = notes.concat(note);
-
-  res.json(note);
+  note
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote);
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.status(500).end();
+    });
 });
 
 const PORT = process.env.PORT;
