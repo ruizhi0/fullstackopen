ee7bc0ae937e2908189758109f85b61f3d9fddb8
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-21 04:32:01 +0800

    chore: setup script and rest test files

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
 
 mongoose.connect(config.MONGODB_URI);
 
