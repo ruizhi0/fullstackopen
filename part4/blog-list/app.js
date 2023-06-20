9bc7b0d58ba8f037f07396b0595b4f9602cae316
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-21 04:25:40 +0800

    refactor: organize code structure

---
+++ part4/blog-list/app.js
@@ -0,0 +1,14 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", blogsRouter);
+
+module.exports = app;
