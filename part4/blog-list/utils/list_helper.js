cb9e49db8079462b755efc4ea67a883ef8751f4d
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-22 03:47:39 +0800

    feat: calculate favourite blog

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -12,7 +12,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  if (blogs.length === 1) {
+    return blogs[0];
+  }
+
+  const reducer = (blogWithMaxLikes, currentBlog) =>
+    currentBlog.likes > blogWithMaxLikes.likes ? currentBlog : blogWithMaxLikes;
+
+  return blogs.reduce(reducer, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
