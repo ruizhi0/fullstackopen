de1effd053a81ae7c2ab4275082902da3b4b0c6a
Liew Rui Zhi <ruizhi@outlook.my>
2023-06-22 03:31:36 +0800

    feat: calculate blogs list total likes

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -7,3 +7,43 @@ test("dummy returns one", () => {
 
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  const emptyList = [];
+
+  const listWithOneBlog = [
+    {
+      totalLikes: 1,
+    },
+  ];
+
+  const listWithManyBlogs = [
+    {
+      totalLikes: 1,
+    },
+    {
+      totalLikes: 2,
+    },
+    {
+      totalLikes: 3,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes(emptyList);
+
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog equals to the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+
+    expect(result).toBe(1);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithManyBlogs);
+
+    expect(result).toBe(6);
+  });
+});
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,4 +2,21 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  if (blogs.length === 1) {
+    return 1;
+  }
+
+  const reducer = (sum, blog) => sum + blog.totalLikes;
+
+  return blogs.reduce(reducer, 0);
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
