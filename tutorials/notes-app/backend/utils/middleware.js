dd19e7fcf4cfe020c905466004aa558ae70f347c
Liew Rui Zhi <ruizhi@outlook.my>
2023-07-04 22:52:02 +0800

    feat: jwt token expiration

--- tutorials/notes-app/backend/utils/middleware.js
+++ tutorials/notes-app/backend/utils/middleware.js
@@ -15,12 +15,15 @@ const unknownEndpoint = (req, res) => {
 const errorHandler = (error, req, res, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
-  } else if (error.name === "JsonWebTokenError") {
-    return res.status(400).json({ error: error.message });
+  switch (error.name) {
+    case "CastError":
+      return res.status(400).send({ error: "malformatted id" });
+    case "ValidationError":
+      return res.status(400).json({ error: error.message });
+    case "JsonWebTokenError":
+      return res.status(400).json({ error: error.message });
+    case "TokenExpiredError":
+      return res.status(401).json({ error: "token expired" });
   }
 
   next(error);
